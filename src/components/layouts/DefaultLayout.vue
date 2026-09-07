c31763a0cea69b85f25be714c1334e35077f362d
fitrahRahmadhani <fitrahrahmadhani2810@gmail.com>
2026-09-07 13:10:15 +0700

    feat: membuat application shell layout

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -4,8 +4,15 @@ import TheSidabar from './TheSidabar.vue'
 </script>
 
 <template>
-  <div class="bg-gray-50">
-    <TheHeader />
-    <TheSidabar />
+  <div class="flex flex-col h-screen bg-gray-50">
+    <TheHeader class="shrink-0" />
+
+    <div class="flex flex-1 overflow-hidden">
+      <TheSidabar class="shrink-0" />
+
+      <main class="flex-1 overflow-y-auto p-8">
+        <slot></slot>
+      </main>
+    </div>
   </div>
 </template>
