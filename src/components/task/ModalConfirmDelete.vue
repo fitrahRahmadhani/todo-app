e65916ef7f1b323d26853b64fba9cdb1c3271212
fitrahRahmadhani <fitrahrahmadhani2810@gmail.com>
2026-09-17 20:03:54 +0700

    refactor: lokalisasi modal store pada delete modal

--- src/components/task/ModalConfirmDelete.vue
+++ src/components/task/ModalConfirmDelete.vue
@@ -1,37 +1,37 @@
 <script setup>
 import { useTaskStore } from '@/stores/taskStore.js'
 import BaseModal from '../ui/BaseModal.vue'
-import { useConfirmDeleteStore } from '@/stores/confirmDeleteStore.js'
+import { useModalStore } from '@/stores/modalStore.js'
 
+const modalStore = useModalStore()
 const taskStore = useTaskStore()
-const confirmDeleteStore = useConfirmDeleteStore()
 
 function confirmDelete(taskId) {
   taskStore.destroyTask(taskId)
-  confirmDeleteStore.closeModal()
+  modalStore.closeModal()
 }
 </script>
 
 <template>
   <BaseModal
     title="Delete this item?"
-    :is-open="confirmDeleteStore.isOpen"
-    @close="confirmDeleteStore.closeModal"
+    :is-open="modalStore.isModalOpen('confirmDelete')"
+    @close="modalStore.closeModal()"
   >
     <p class="text-gray-600">
       Are you sure you’d like to remove
-      <span class="font-semibold">{{ confirmDeleteStore.targetTask.title }}</span
+      <span class="font-semibold">{{ modalStore.payload?.title }}</span
       >? Once deleted, it can’t be restored.
     </p>
     <div class="w-full flex mt-10 gap-4 items-center justify-end">
       <button
-        @click="confirmDeleteStore.closeModal"
+        @click="modalStore.closeModal()"
         class="px-4 py-2 text-sm font-medium text-gray-600 rounded-lg hover:bg-gray-100 transition-colors"
       >
         Cancel
       </button>
       <button
-        @click="confirmDelete(confirmDeleteStore.targetTask.id)"
+        @click="confirmDelete(modalStore.payload?.id)"
         class="px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-600 transition-colors"
       >
         Delete
