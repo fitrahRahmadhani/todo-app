29cc3576495093a2901e2e0c80c225b443581156
fitrahRahmadhani <fitrahrahmadhani2810@gmail.com>
2026-09-20 09:28:01 +0700

    feat: menambahkan halaman all task

--- src/views/AllTasksView.vue
+++ src/views/AllTasksView.vue
@@ -1,10 +1,13 @@
 <script setup>
 import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
 import TaskLists from '@/components/task/TaskLists.vue'
+import { useTaskStore } from '@/stores/taskStore'
+
+const taskStore = useTaskStore()
 </script>
 
 <template>
   <DefaultLayout titlePage="All Task">
-    <TaskLists />
+    <TaskLists :tasks="taskStore.tasks" />
   </DefaultLayout>
 </template>
