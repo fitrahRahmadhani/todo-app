bacf0b60a258b28b44b683083767a63d9987b744
fitrahRahmadhani <fitrahrahmadhani2810@gmail.com>
2026-09-20 10:26:52 +0700

    fix: memperbaiki bug minimize sidebar

--- src/components/layouts/TheSidabar.vue
+++ src/components/layouts/TheSidabar.vue
@@ -9,12 +9,10 @@ import {
   X,
 } from '@lucide/vue'
 import BaseNavLink from '../ui/BaseNavLink.vue'
-import { ref } from 'vue'
 import { useSidebarStore } from '@/stores/sidebarStore'
 
 defineOptions({ inheritAttrs: false })
 
-const isMinimize = ref(false)
 const sidebarStore = useSidebarStore()
 
 function handleNavClick() {
@@ -34,7 +32,7 @@ function handleNavClick() {
     v-bind="$attrs"
     class="fixed md:static top-0 left-0 md:inset-auto z-40 md:z-auto pt-16 px-6 h-full border-r border-gray-200 bg-white shrink-0 transition-transform md:transition-[width] duration-300 ease-in-out"
     :class="[
-      isMinimize ? 'md:w-24' : 'md:w-60',
+      sidebarStore.isMinimize ? 'md:w-24' : 'md:w-60',
       'w-64',
       sidebarStore.isOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0',
     ]"
@@ -51,41 +49,41 @@ function handleNavClick() {
     <!-- Minimize toggle, desktop only -->
     <button
       class="hidden md:block absolute -right-4 top-4 bg-white text-gray-400 p-2 border border-gray-200 rounded-xl hover:text-gray-600 hover:bg-gray-50 transition duration-300"
-      @click="isMinimize = !isMinimize"
+      @click="sidebarStore.isMinimize = !sidebarStore.isMinimize"
     >
-      <PanelRightOpen :size="18" v-if="!isMinimize" />
-      <PanelRightClose :size="18" v-if="isMinimize" />
+      <PanelRightOpen :size="18" v-if="!sidebarStore.isMinimize" />
+      <PanelRightClose :size="18" v-if="sidebarStore.isMinimize" />
     </button>
     <nav class="space-y-1" @click="handleNavClick">
       <p
         class="uppercase text-xs text-gray-400 font-semibold mb-2"
-        :class="isMinimize ? 'invisible' : 'visible'"
+        :class="sidebarStore.isMinimize ? 'invisible' : 'visible'"
       >
         workspace
       </p>
-      <BaseNavLink :to="{ name: 'today' }" :is-minimize="isMinimize">
+      <BaseNavLink :to="{ name: 'today' }" :is-minimize="sidebarStore.isMinimize">
         <template v-slot:icon>
           <Sun :size="18" />
         </template>
-        <span v-if="!isMinimize">Today</span>
+        <span v-if="!sidebarStore.isMinimize">Today</span>
       </BaseNavLink>
-      <BaseNavLink :to="{ name: 'all-tasks' }" :is-minimize="isMinimize">
+      <BaseNavLink :to="{ name: 'all-tasks' }" :is-minimize="sidebarStore.isMinimize">
         <template v-slot:icon>
           <Inbox :size="18" />
         </template>
-        <span v-if="!isMinimize">All Tasks</span>
+        <span v-if="!sidebarStore.isMinimize">All Tasks</span>
       </BaseNavLink>
-      <BaseNavLink :to="{ name: 'upcoming' }" :is-minimize="isMinimize">
+      <BaseNavLink :to="{ name: 'upcoming' }" :is-minimize="sidebarStore.isMinimize">
         <template v-slot:icon>
           <CalendarClock :size="18" />
         </template>
-        <span v-if="!isMinimize">Upcoming</span>
+        <span v-if="!sidebarStore.isMinimize">Upcoming</span>
       </BaseNavLink>
-      <BaseNavLink :to="{ name: 'completed' }" :is-minimize="isMinimize">
+      <BaseNavLink :to="{ name: 'completed' }" :is-minimize="sidebarStore.isMinimize">
         <template v-slot:icon>
           <CircleCheckBig :size="18" />
         </template>
-        <span v-if="!isMinimize">Upcoming</span>
+        <span v-if="!sidebarStore.isMinimize">Completed</span>
       </BaseNavLink>
     </nav>
   </aside>
